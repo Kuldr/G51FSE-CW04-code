e5c7d842eff5ece63057624e4515fc3939eec6f7
Oscar Mason <oscar@live.co.uk>
2016-03-11 15:14:00 +0000

    Changed user input tests

--- src/WorkshopReviewSystemTest.java
+++ src/WorkshopReviewSystemTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
 
 public class WorkshopReviewSystemTest {
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    private final String optionText = "What do you want to do?\n O = Overview, P = Add Paper, R = Add Review, [num] = Detail of that paper, X = exit\n";
+    private final String optionText = "What do you want to do?\n O = Overview, P = Add Paper, R = Add Review, [num] = Detail of that paper, X = exit"+System.lineSeparator();
     //private final ByteArrayInputStream in = new ByteArrayInputStream();
     @Before
     public void setUpStreams(){
@@ -36,7 +36,10 @@ public class WorkshopReviewSystemTest {
 
         workshopReviewSystem.main(new String[]{""});
         float averageScore = 4/3;
-        String output = optionText + "\n1) Paper 1 is great - 4.0\n2) Paper 2 is my best work - " + averageScore + "\n"+ optionText;
+        //TODO Hardcoded average score
+        String output = optionText + "1) Paper 1 is great - 4.0" + System.lineSeparator() +
+                "2) Paper 2 is my best work - 2.0" + System.lineSeparator() +
+                optionText;
         assertEquals(output, outContent.toString());
     }
 
@@ -51,10 +54,8 @@ public class WorkshopReviewSystemTest {
         System.setIn(in);
 
         workshopReviewSystem.main(new String[]{""});
-        String output = optionText + "What is the title of the paper?\n[Paper added]\n"+ optionText;
-        output = output.replace("\n","").replace("\r","");
-        String outTest = outContent.toString().replace("\n","").replace("\r","");
-        assertEquals(output, outTest );
+        String output = optionText + "What is the title of the paper?"+System.lineSeparator()+"[Paper added]"+System.lineSeparator()+ optionText;
+        assertEquals(output, outContent.toString() );
     }
 
 
@@ -69,9 +70,11 @@ public class WorkshopReviewSystemTest {
         System.setIn(in);
 
         workshopReviewSystem.main(new String[]{""});
-        String output = optionText + "\nWhat is the title of the paper?\n"+ optionText;
-        output = output.replace("\n","").replace("\r","");
-        String outTest = outContent.toString().replace("\n","").replace("\r","");
-        assertEquals(output, outTest );
+        String output = optionText + "Which paper do you want to add a review to?" + System.lineSeparator() +
+                "What score do you give it?" + System.lineSeparator() +
+                "Please enter your review:" + System.lineSeparator() +
+                "[Review added to Paper 2]" + System.lineSeparator() +
+                optionText;
+        assertEquals(output, outContent.toString());
     }
 }
